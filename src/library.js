376cc8f72a06fa5638cccdd7d52f4bec370e6df7
tomebee0 <tomebee0@gmail.com>
2019-02-04 20:52:55 +0100

    Basic Linq extension methods

--- src/library.js
+++ src/library.js
@@ -1,10 +1,96 @@
 import { getLibName } from "./utils";
 
-export default class Library {
-  constructor() {
-    this._name = getLibName();
+export default (function() {
+  const checkNullOrEmptyArray = function(array) {
+    if(!array || array.length < 1) {
+      throw 'array is empty';
+    }
   }
-  get name() {
-    return this._name;
+  const throwIfNotFunction = function(param, errorMessage) {
+    if(!param || {}.toString.call(param) !== '[object Function]') {
+      throw errorMessage || 'parameter is not a function';
+    }
   }
-}
+
+  Array.prototype.select = function(mapFunction) {
+    throwIfNotFunction(mapFunction);
+    let array = this;
+    checkNullOrEmptyArray(array);
+    let elements;
+    for(let element of array) {
+      if(!elements) {
+        elements = [];
+      }
+      elements.push(mapFunction(element));
+    }
+
+    return elements;
+  };
+
+  Array.prototype.where = function(predicate) {
+    throwIfNotFunction(predicate,'predicate is not a function')
+
+    let array = this;
+    checkNullOrEmptyArray(array);
+    let elements;
+    for(let element of array) {
+      if(!elements) {
+        elements = [];
+      }
+      let predicateResult = predicate(element);
+      if(typeof predicateResult != 'boolean') {
+        throw 'predicate result must be boolean type';
+      }
+      if(predicateResult) {
+        elements.push(element);
+      }
+    }
+    return elements;
+  };
+
+  Array.prototype.firstOrDefault = function(predicate) {
+    throwIfNotFunction(predicate,'predicate is not a function');
+    let array = this;
+    checkNullOrEmptyArray(array);
+    let result;
+    for(let element of array) {
+      if(result)
+        break;
+
+      let predicateResult = predicate(element);
+
+      if(typeof predicateResult != 'boolean') {
+        throw 'predicate result must be boolean type';
+      }
+
+      if(predicateResult) {
+        result = element;
+      }
+    }
+    return result;
+  };
+
+  Array.prototype.first = function(predicate) {
+    let array = this;
+    checkNullOrEmptyArray(array);
+    if(!predicate) {
+      return array[0];
+    }
+    let element = array.firstOrDefault(predicate);
+    if(!element) {
+      throw 'Element not found by provided predicate';
+    }
+    return element;
+  };
+
+  Array.prototype.any = function(predicate) {
+    let array = this;
+    if(!array || array.length < 1) {
+      return false;
+    }
+    if(!predicate) {
+      return array.length > 0;
+    }
+    return array.firstOrDefault(predicate) ? true : false;
+  }
+})();
